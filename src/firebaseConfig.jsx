e343bea5e5a0bcefd13ec3a090bfe9ee846a1230
Kevin11282003 <128556228+Kevin11282003@users.noreply.github.com>
2025-03-31 23:47:00 +0000

    base

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCvTpYbMvjVW_wQ_9pVk08l8SQ6azP4OVk",
-    authDomain: "melo-37ab5.firebaseapp.com",
-    projectId: "melo-37ab5",
-    storageBucket: "melo-37ab5.firebasestorage.app",
-    messagingSenderId: "587191030710",
-    appId: "1:587191030710:web:888373e2e71cc411592717"
-  };
+  apiKey: "AIzaSyA70h_1NC8iuA2McQDwMCz6Y4U7YPjcRkI",
+  authDomain: "datos-57ac7.firebaseapp.com",
+  projectId: "datos-57ac7",
+  storageBucket: "datos-57ac7.firebasestorage.app",
+  messagingSenderId: "480042261394",
+  appId: "1:480042261394:web:bf8e5f521c7b6c634cff4b"
+};   
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
